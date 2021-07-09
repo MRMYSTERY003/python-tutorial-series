3b237a12f917e5e18d546f7c131e7f695e7c1544
Sriram <80098044+MRMYSTERY003@users.noreply.github.com>
2021-07-09 15:30:34 +0530

    basic datatypes in python

---
+++ python basic data types.py
@@ -0,0 +1,23 @@
+# the basic datatypes availabe in python are followed:
+
+#  1. int = integer(ex. 1, 4,59,0,2,-42,-4 ) all the number from - infinity to + infinity (but all of them are whole numbers)
+#  2.float = floating points (or decimal numbers) (ex. 1.0 ,340.32, 0.0, -0.4320, ect) all the number from - infinity to + infinity (but all of them are decimal numbers)
+#  3.bool = boolean it is a special data type which can only have either True or False 
+#  4.str = string litrally anything given between quetotation is known as a string .(ex. 'code hub', "5323", "0423.42", '''True''') we can use single quotation (''), double ("") or triple quotation(''' ''')
+  
+#  so these are the four basic datatypes available in python so let's see some example program
+
+
+# creating 4 variables and storing different datatypes in them
+num = 432               #integer
+decimal = 20.423        #float
+boolean = True          #boolean
+string = 'litrally anything... inside this quotation'     #sting
+
+#let's see their datatypes in program , we use type() to know the datatype of a variable
+print(type(num))
+print(type(decimal))
+print(type(boolean))
+print(type(string))
+
+
